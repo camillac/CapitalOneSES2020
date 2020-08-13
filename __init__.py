2676b8446e7710fffb90f39c19da0aaa4a0bd402
camillac <camilla.cc19@gmail.com>
2020-08-13 19:28:26 -0400

    added graphs

--- __init__.py
+++ __init__.py
@@ -12,6 +12,26 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mymusic.db'
 db = SQLAlchemy(app)
 api_key = 'dceced5acb16100198959ed0cfa3d62b'
 
+labels = [
+    'Take Out',
+    'Groceries',
+    'Entertainment',
+    'Misc'
+]
+
+values = [
+    967.67, 1190.89, 1079.75, 1349.19
+]
+
+colors = [
+    "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA"
+]
+
+@app.route('/graphs')
+def bar():
+    bar_labels=labels
+    bar_values=values
+    return render_template('graphs.html', title='Monthly Expenses', max=17000, labels=bar_labels, values=bar_values)
 
 @app.route('/')
 def main():
